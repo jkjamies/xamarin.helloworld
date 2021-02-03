464d2aabbbb93373235e4224edad4c1ae490d5f4
Jason Jamieson <jjamieson@OST-C02DN1D3MD6M.localdomain>
2021-02-03 16:22:40 -0500

    initial commit

---
+++ HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DisplayAlert("Title", "Hello World", "Ok");
+        }
+    }
+}
